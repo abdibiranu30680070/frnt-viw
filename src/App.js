cf0484f1c6f96c635f3ea8342ccff5e2e84f0520
abdibiranu30680070 <153322769+abdibiranu30680070@users.noreply.github.com>
2025-05-24 22:16:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignupPage from './pages/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/Home';
 import PageNotFound from './components/PageNotFound';
 import AdminDashboard from "./pages/AdminDashboard";
